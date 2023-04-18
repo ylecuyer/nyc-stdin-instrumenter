a807a763f4fb337a1a188cc10fdd73e525664c9d
Yoann Lecuyer <yoann.lecuyer@gmail.com>
2023-04-18 18:44:35 +0200

    es module false to avoid strict mode

--- bin/nyc-stdin-instrumenter.js
+++ bin/nyc-stdin-instrumenter.js
@@ -5,7 +5,9 @@ var fs = require('fs')
 
 var data = fs.readFileSync(0, 'utf-8')
 
-var instrumenter = createInstrumenter({})
+var instrumenter = createInstrumenter({
+  esModules: false
+})
 
 // argv[2] is the path of the file
 output = instrumenter.instrumentSync(data, process.argv[2])
